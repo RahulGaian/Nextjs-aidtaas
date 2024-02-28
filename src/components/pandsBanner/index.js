0062171f1ca2549b74412cac2118b402eb541b80
Shubham Lahase <shubham.sheshrao@gaiansolutions.com>
2024-02-28 16:54:24 +0530

    imagesnew folder added and components folder added

---
+++ src/components/pandsBanner/index.js
@@ -0,0 +1,11 @@
+import st from "./index.module.css";
+
+const PandSBanner = () => {
+    return(
+        <div className={st.container}>
+            <div className={st.contentContainer}>
+                
+            </div>
+        </div>
+    )
+}

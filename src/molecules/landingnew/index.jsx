f6fabed806a27978a7f528809fe54e4b980215d7
hussainhafidh <hussainhafidh1@gmail.com>
2024-03-14 16:48:45 +0530

    updated LandingPage

--- src/molecules/landingnew/index.jsx
+++ src/molecules/landingnew/index.jsx
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 import React from "react";
 import Topsection from "./topsection";
 import Section2 from "./secondsectionnew";
-import St from "./index.module.css"
-const Landingpagenew=()=>{
-    return (
-        <div className={St.landmain}>
-           <Topsection></Topsection>
-           <Section2></Section2>
+import St from "./index.module.css";
+const Landingpagenew = () => {
+  return (
+    <div className={St.landmain}>
+      <div className={St.bg_blueBlob1}>
+        <div className={St.bg_pink_color_1}>
+          <div className={St.bg_blueBlob2}>
+            <Topsection></Topsection>
+            <Section2></Section2>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Landingpagenew;
--- src/molecules/textAndImage/index.jsx
+++ src/molecules/textAndImage/index.jsx
@@ -1,51 +1,45 @@
 // import { useEffect,useState } from "react";
 // import "./index.css"
-import st from "./index.module.css"
-import {default as Img} from "next/image";
-const TextandImage = ({text1,text2,img,side}) => {
-
-        // const [side,setSide] = useState("right")
-        // let display_width = window.innerWidth;
-        // // if(display_width<500) setSide("right") 
-        // // if(display_width>500) setSide("left")
-    if(side ==="right") {
-
-            return( 
-            <div className={st.TextImg}>
-
-           
-                <div className={st.contright + " pt-8"} >
-                    <div className={st.cont1}>
-                        <h3 className={st.T1}>{text1}</h3>
-                        <p className={st.T2}>{text2}</p>
-                    </div>
-                    <div className={st.cont2}>
-                    <Img priority fill src={img} />
-                    </div>
-                </div>
-            </div>
-        )
-    
-    }
-    if (side === "left"){
-        return( 
-            <div className={st.TextImg}>
-
-            
-            <div className={st.contleft + " pt-8"}  >
-                <div className={st.cont2} >
-                <Img priority fill src={img} />
-
-                </div>
-                
-                <div className={st.cont1}>
-                <h3 className={st.T1}>{text1}</h3>
-                        <p className={st.T2}>{text2}</p>
-                </div>
-            </div>
+import st from "./index.module.css";
+import { default as Img } from "next/image";
+const TextandImage = ({ text1, text2, img, side }) => {
+  // const [side,setSide] = useState("right")
+  // let display_width = window.innerWidth;
+  // // if(display_width<500) setSide("right")
+  // // if(display_width>500) setSide("left")
+  if (side === "right") {
+    return (
+      <div className={st.TextImg}>
+        <div className={st.contright + " pt-8"}>
+          <div className={st.cont1}>
+            <h3 className={st.T1}>{text1}</h3>
+            <p className={st.T2}>{text2}</p>
+          </div>
+          <div className={st.cont2}>
+            <Img priority fill src={img} />
+          </div>
         </div>
-    )
-    }
-}
+      </div>
+    );
+  }
+  if (side === "left") {
+    return (
+      <div className={st.TextImg}>
+        {/* <div className={st.bg_blue_color_1}> */}
+          <div className={st.contleft + " pt-8"}>
+            <div className={st.cont2}>
+              <Img priority fill src={img} />
+            </div>
+
+            <div className={st.cont1}>
+              <h3 className={st.T1}>{text1}</h3>
+              <p className={st.T2}>{text2}</p>
+            </div>
+          {/* </div> */}
+        </div>
+      </div>
+    );
+  }
+};
 
 export default TextandImage;

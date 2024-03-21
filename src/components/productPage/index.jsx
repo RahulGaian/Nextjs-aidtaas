0725c8c9d25a0472b894dddfec8a9a7e98dac5ba
hussainhafidh <hussainhafidh1@gmail.com>
2024-03-21 17:43:57 +0530

    productPage updated

--- src/components/productPage/index.jsx
+++ src/components/productPage/index.jsx
@@ -18,35 +18,38 @@ const ProductPage = ({ data }) => {
       style={{
         // background:
         //   "linear-gradient(90deg, #bfeeff 0%, #ffe6f7 56.93%, #ffe6e6 80%)",
-          background: "linear-gradient(109deg, #d2f3ff 0%, #ffe3e3 100%)",
-
+        background: "linear-gradient(109deg, #d2f3ff 0%, #ffe3e3 100%)",
       }}
     >
       <section>
-        {/* <ProductsNSolutionBanner bannerData={data.banner} /> */}
-        <ProductBanner bannerData={data.banner} /> 
-
+        <ProductBanner bannerData={data.banner} />
       </section>
 
-      <section>
-        <HeroImageNQuickJump data={data.banner} />
-      </section>
+      <div className={styles.bg_blue_color_1}>
+        <section>
+          <HeroImageNQuickJump data={data.banner} />
+        </section>
 
-      <section>
-        <Features featureData={data.features} />
-      </section>
+        <div className={styles.bg_pink_blob_1}>
+          <div className={styles.bg_blue_color_2}>
+            <section>
+              <Features featureData={data.features} />
+            </section>
 
-      <section>
-        <BenefitsComponent2 benefitsData={data.benefits} />
-      </section>
+            <section>
+              <BenefitsComponent2 benefitsData={data.benefits} />
+            </section>
 
-      <section>
-        <AppSwiper />
-      </section>
+            <section>
+              <AppSwiper />
+            </section>
 
-      <section>
-        <ContactUs data={contactUsData.contact3} />
-      </section>
+            <section>
+              <ContactUs data={contactUsData.contact3} />
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
